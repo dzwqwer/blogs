cc337cec404ae069b1c3fb9f4b7b8e57ef27800f
dengzhiwei <18390242656@163.com>
2021-01-11 15:13:20 +0800

    [update] update config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  title: 'Hello VuePress',
-  description: 'Just playing around',
+  title: '我的主页', // 显示在左上角的网页名称以及首页在浏览器标签显示的title名称
+  description: 'Just playing around', // meta 中的描述文字，用于SEO
   base: '/blogs/',
 }
